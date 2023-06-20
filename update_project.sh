b5769eab0c2633ddc54bea3557305b36f412ffd9
chh59ja <christian.hauptmann@uni-wuerzburg.de>
2023-06-20 23:26:44 +0200

    add requirements install

--- update_project.sh
+++ update_project.sh
@@ -10,8 +10,13 @@ pkill gunicorn
 # activate your virtual environment, replace 'venv' with the name of your virtual environment
 source venv/bin/activate
 
+
+
 # navigate to the server folder and start the gunicorn service
 cd ./server
+
+# install the latest requirements
+pip install -r requirements.txt
 
 # start the gunicorn service
 gunicorn --bind 0.0.0.0:5000 wsgi:app --access-logfile logs/gunicorn_access.log --error-logfile logs/gunicorn_error.log --log-level info --daemon
