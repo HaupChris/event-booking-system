e396994dfc27d6fb3c9d2266d0fb5cbcc836f31a
chh59ja <christian.hauptmann@uni-wuerzburg.de>
2024-02-29 22:03:47 +0100

    create new theme for "underwater"

--- frontend/event-booking-system/src/App.tsx
+++ frontend/event-booking-system/src/App.tsx
@@ -4,114 +4,129 @@ import PasswordPage from "./form/passwordPage";
 import {AuthContext, TokenContext} from "./AuthContext";
 import {FormContainer} from "./form/formContainer";
 import {Box, createTheme} from "@mui/material";
-import backgroundImage from './img/background_desktop.jpeg';
+import backgroundImageDesktop from './img/background_desktop_water.png';
+import backgroundImageMobile from './img/background_mobile_water.png';
+
 
 import {ThemeOptions, ThemeProvider} from '@mui/material/styles';
 import AdminDashboard from "./form/admin/adminDashboard";
 import AdminLogin from './form/adminLogin';
 
+import './css/global.css';
+
 
 export const themeOptions: ThemeOptions = {
-	palette: {
-		mode: 'dark',
-		primary: {
-			main: '#fef43f',
-		},
-		secondary: {
-			main: '#e65813',
-		},
-		background: {
-			default: '#0f3233',
-			paper: '#031a2a',
-		},
-		divider: '#f6500c',
-		text: {
-			primary: '#a29c0d',
-			secondary: '#817c0e',
-		},
-
-	},
-	typography: {
-		fontFamily: 'Permanent Marker',
-		body1: {
-			fontFamily: 'Kavoon',
-		},
-		h1: {
-			fontFamily: 'Kavoon',
-		},
-		button: {
-			fontFamily: 'Kavoon',
-		},
-	},
-	components: {
-		MuiCard: {
-			styleOverrides: {
-				root: {
-					fontFamily: 'Kavoon',
-				},
-			},
-		},
-	},
+  palette: {
+    mode: 'light', // Switching to light mode for a friendlier vibe
+    primary: {
+      main: 'rgb(2,79,152)', // Keeping white for legibility
+    },
+    secondary: {
+      main: 'rgba(0,0,0,0.9)', // A brighter yellow for vibrancy
+    },
+    background: {
+      default: 'rgba(255, 255, 255, 0.3)', // Light translucent background for form
+      paper: 'rgba(255, 255, 255, 0.7)', // Lighter for paper elements with less transparency
+    },
+    divider: 'rgba(30, 144, 255, 0.5)', // Using a sky blue for a friendlier divider color
+    text: {
+      primary: 'rgb(2,79,152)', // Dark text for contrast and readability
+      secondary: 'rgba(0, 0, 0, 0.7)', // Slightly dimmed for secondary text
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Kavoon',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    body1: {
+      fontFamily: 'Kavoon',
+    },
+    h1: {
+      fontFamily: 'Kavoon',
+    },
+    button: {
+      fontFamily: 'Kavoon',
+    },
+  },
+  components: {
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'Kavoon',
+          backgroundColor: 'rgba(255, 255, 255, 0.1)', // Very light background for card elements
+          backdropFilter: 'blur(5px)', // Optional: Apply a blur effect to the background
+        },
+      },
+    },
+    // If you use other components that need to be styled, add them here
+  },
 };
+
 const theme = createTheme(themeOptions);
 const documentHeight = () => {
-	const doc = document.documentElement
-	doc.style.setProperty('--doc-height', `${window.innerHeight - 8}px`)
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight - 8}px`)
 }
 
 const App = () => {
-	const [auth, setAuth] = useState(false);
-	const [isAdmin, setIsAdmin] = useState(false);
-	const [token, setToken] = useState("");
+    const [auth, setAuth] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [token, setToken] = useState("");
 
-	// Restore token from local storage at startup
-	useEffect(() => {
-		const storedToken = window.localStorage.getItem("token");
-		if (storedToken) {
-			setToken(storedToken);
-			setAuth(true);
-		}
+    // Restore token from local storage at startup
+    useEffect(() => {
+        const storedToken = window.localStorage.getItem("token");
+        if (storedToken) {
+            setToken(storedToken);
+            setAuth(true);
+        }
 
-		window.addEventListener('resize', documentHeight);
-		documentHeight()
-	}, []);
+        window.addEventListener('resize', documentHeight);
+        documentHeight()
+    }, []);
 
-	// Save token to local storage whenever it changes
-	useEffect(() => {
-		if (token) {
-			window.localStorage.setItem("token", token);
-		}
-	}, [token]);
+    // Save token to local storage whenever it changes
+    useEffect(() => {
+        if (token) {
+            window.localStorage.setItem("token", token);
+        }
+    }, [token]);
 
-	return <ThemeProvider theme={theme}>
-		<Box
-			sx={{
-				position: 'fixed',
-				top: 0,
-				left: 0,
-				width: '100vw',
-				height: '100vh',
-				backgroundImage: `url(${backgroundImage})`,
-				backgroundSize: 'cover',
-				backgroundPosition: 'center',
-			}}
-		>
-			<AuthContext.Provider value={{auth, setAuth, isAdmin, setIsAdmin}}>
-				<TokenContext.Provider value={{token, setToken}}>
-					<BrowserRouter>
-						<Routes>
-							<Route path="/" element={auth ? <Navigate replace to="/form"/> : <PasswordPage/>}/>
-							<Route path="/form" element={auth ? <FormContainer/> : <Navigate replace to="/"/>}/>
-							<Route path="/admin"
-								   element={isAdmin ? <Navigate replace to="/admin/dashboard"/> : <AdminLogin/>}/>
-							<Route path="/admin/dashboard"
-								   element={isAdmin ? <AdminDashboard/> : <Navigate replace to={"/admin"}/>}/>
-						</Routes>
-					</BrowserRouter>
-				</TokenContext.Provider>
-			</AuthContext.Provider>
-		</Box>
-	</ThemeProvider>
+    return <ThemeProvider theme={theme}>
+        <Box
+            sx={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100vw',
+                height: '100vh',
+                // backgroundImage: `url(${backgroundImage})`,
+                // check for mobile
+                backgroundImage: `url(${window.innerWidth < 600 ? backgroundImageMobile : backgroundImageDesktop})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <AuthContext.Provider value={{auth, setAuth, isAdmin, setIsAdmin}}>
+                <TokenContext.Provider value={{token, setToken}}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={auth ? <Navigate replace to="/form"/> : <PasswordPage/>}/>
+                            <Route path="/form" element={auth ? <FormContainer/> : <Navigate replace to="/"/>}/>
+                            <Route path="/admin"
+                                   element={isAdmin ? <Navigate replace to="/admin/dashboard"/> : <AdminLogin/>}/>
+                            <Route path="/admin/dashboard"
+                                   element={isAdmin ? <AdminDashboard/> : <Navigate replace to={"/admin"}/>}/>
+                        </Routes>
+                    </BrowserRouter>
+                </TokenContext.Provider>
+            </AuthContext.Provider>
+        </Box>
+    </ThemeProvider>
 };
 
 export default App;
