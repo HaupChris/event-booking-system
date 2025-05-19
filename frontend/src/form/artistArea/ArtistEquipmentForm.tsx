d7000b10ba99fede1821781c4e568711bc4aa795
chh59ja <christian.hauptmann@uni-wuerzburg.de>
2025-05-19 22:49:59 +0200

    update Artist Equipment

--- frontend/src/form/artistArea/ArtistEquipmentForm.tsx
+++ frontend/src/form/artistArea/ArtistEquipmentForm.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { ArtistFormProps } from './ArtistRegistrationFormContainer';
+import {Box, Typography} from '@mui/material';
+import {ArtistFormProps} from './ArtistRegistrationFormContainer';
 import SpacePanelLayout from '../../components/core/layouts/SpacePanelLayout';
-import { artistAreaTexts } from '../constants/texts';
+import {artistAreaTexts} from '../constants/texts';
 import SettingsIcon from '@mui/icons-material/Settings';
 import MissionHeading from '../../components/core/display/MissionHeading';
 import FormField from '../../components/core/inputs/FormField';
@@ -16,11 +16,11 @@ function ArtistEquipmentForm(props: ArtistFormProps) {
         >
             <MissionHeading
                 title={artistAreaTexts.technicalRequirementsForm.title}
-                icon={<SettingsIcon />}
+                icon={<SettingsIcon/>}
             />
 
             <FormCard>
-                <Box sx={{ px: 2, pb: 2 }}>
+                <Box sx={{px: 2, pb: 2}}>
                     <FormField
                         multiline
                         rows={5}
@@ -45,29 +45,37 @@ function ArtistEquipmentForm(props: ArtistFormProps) {
 
             <FormCard
                 title="Verfügbare Ausrüstung am Veranstaltungsort"
-                sx={{ mt: 3 }}
+                sx={{mt: 3}}
             >
-                <Box sx={{ px: 2, pb: 2 }}>
+                <Box sx={{px: 2, pb: 2}}>
                     <Typography variant="body2" paragraph>
                         Folgendes Equipment steht grundsätzlich zur Verfügung (Details können variieren):
                     </Typography>
 
-                    <Typography variant="subtitle2" sx={{ mt: 2 }}>Hauptbühne:</Typography>
+                    <Typography variant="subtitle2" sx={{mt: 2}}>Hauptbühne:</Typography>
                     <ul>
                         <li>XDJ-XZ</li>
                         <li>1x XDJ 1000</li>
                         <li>1x Reloop rp7000</li>
                     </ul>
 
-                    <Typography variant="subtitle2" sx={{ mt: 2 }}>Bandbühne:</Typography>
+                    <Typography variant="subtitle2" sx={{mt: 2}}>Bandbühne:</Typography>
                     <ul>
-                        <li>Yamaha Stagepas 1K Stereo Bundle</li>
-                        <li>Behringer XR18 Wireless Mixer</li>
-                        <li>Standard Mikrofone (SM58 o.ä.)</li>
+                        <li><a href="https://www.thomann.de/de/yamaha_stagepas_1k_stereo_bundle.htm" target="_blank"
+                               style={{color: "#00ffff"}}>
+                            Yamaha Stagepas 1K Stereo Bundle
+                        </a></li>
+                        <li>
+                            <a href="https://www.behringer.com/product.html?modelCode=0605-AAD" target="_blank"
+                               style={{color: "#00ffff"}}>
+                                Behringer XR18 Wireless Mixer
+                            </a>
+                        </li>
                     </ul>
 
-                    <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
-                        Bitte beachte: Spezielle Ausrüstung muss selbst mitgebracht werden oder im Vorfeld abgestimmt werden.
+                    <Typography variant="body2" color="text.secondary" sx={{mt: 2}}>
+                        Bitte beachte: Spezielle Ausrüstung muss selbst mitgebracht werden oder im Vorfeld abgestimmt
+                        werden.
                     </Typography>
                 </Box>
             </FormCard>
